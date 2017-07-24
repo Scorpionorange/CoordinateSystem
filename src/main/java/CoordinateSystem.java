11bc81fd0f3b0ba2b4588fb85f8731d231d6b564
scorpionorange <scorpionorange@126.com>
2017-07-24 23:54:42 +0800

    Initial commit

---
+++ src/main/java/CoordinateSystem.java
@@ -0,0 +1,15 @@
+/**
+ * Created by ScorpionOrange on 2017/07/24.
+ */
+public class CoordinateSystem {
+    // Do nothing yet, but soon!
+
+    //默认构造函数
+    public CoordinateSystem(){}
+
+    //整形构造函数（接受整形坐标的数组）
+    public CoordinateSystem(int[] inputArray){}
+
+    //浮点构造函数（接受浮点坐标的数组）
+    public CoordinateSystem(double[] inputArray){}
+}
---
+++ src/main/java/MainFrame.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ScorpionOrange on 2017/07/24.
+ */
+public class MainFrame {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
