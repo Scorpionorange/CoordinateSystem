652ae35d7e2ba144d17b217e289490d5a57297f2
scorpionorange <scorpionorange@126.com>
2017-09-14 00:55:43 +0800

    update at 2017.9.14;

--- src/main/java/CoordinateSystem.java
+++ src/main/java/CoordinateSystem.java
@@ -50,25 +50,40 @@ class CoordinateSystemComponent extends JComponent{
     Point2D topMiddle = new Point2D.Double(screenWidth / 2, 0);
     Point2D bottomMiddle = new Point2D.Double(screenWidth / 2, screenHeight - 75);
 
-    public void paintComponent(Graphics graphics){
+    int X0 = (int)center.getX();
+    int Y0 = (int)center.getY();
+
+    public void paintComponent(Graphics graphics) {
         Graphics2D graphics2D = (Graphics2D) graphics;
 
         // draw X-axis
-        graphics2D.draw(new Line2D.Double(leftMiddle,rightMiddle));
+        graphics2D.draw(new Line2D.Double(leftMiddle, rightMiddle));
 
         // draw string "+X" and "-X"
-        graphics2D.drawString("+X", (int)rightMiddle.getX() - 10, (int)rightMiddle.getY() - 5);
-        graphics2D.drawString("-X", (int)leftMiddle.getX() + 10, (int)leftMiddle.getY() + 10);
+        graphics2D.drawString("+X", (int) rightMiddle.getX() - 10, (int) rightMiddle.getY() - 5);
+        graphics2D.drawString("-X", (int) leftMiddle.getX() + 10, (int) leftMiddle.getY() + 10);
 
         // draw Y-axis
         graphics2D.draw(new Line2D.Double(topMiddle, bottomMiddle));
 
         // draw string "+Y" and "-Y"
-        graphics2D.drawString("+Y", (int)topMiddle.getX() + 5, (int)topMiddle.getY() + 10);
-        graphics2D.drawString("-Y", (int)bottomMiddle.getX() - 15, (int)bottomMiddle.getY());
+        graphics2D.drawString("+Y", (int) topMiddle.getX() + 5, (int) topMiddle.getY() + 10);
+        graphics2D.drawString("-Y", (int) bottomMiddle.getX() - 15, (int) bottomMiddle.getY());
 
-        // draw a line
-        graphics2D.drawLine((int)center.getX(), (int)center.getY(), (int)center.getX() + 100, (int)center.getY() - 100);
+        // draw a testing line
+        //graphics2D.drawLine(X0, Y0, X0 + 100, Y0 - 100);
+
+        // testing F(x)
+        for(int i = 0; i <= 150; i++){
+            graphics2D.drawLine(X0 + i, F(X0 +i), X0 + i, F(X0 - i));
+        }
+
+    }
+
+    // y = F(x) = 3x + 2;
+    public int F(int x) {
+        int y = 3 * x + 2;
+        return y;
     }
 
     public Dimension getPreferreiSize(){
